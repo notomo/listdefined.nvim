80116039b2cc6ef5806489dfe26921da41d6df76
notomo <notomo.motono@gmail.com>
2023-02-24 09:46:36 +0900

    Add type doc

--- lua/listdefined/init.lua
+++ lua/listdefined/init.lua
@@ -1,9 +1,14 @@
 local M = {}
 
+--- @class ListdefinedItem
+--- @field path string defined file path
+--- @field start_row integer defined position start row
+--- @field text string
+
 --- Returns keymap positions defined in the files.
 --- This function targets |vim.keymap.set()|.
 --- @param paths string[]: file paths
---- @return table: keymap positions {path: string, start_row: number, text: string}[]
+--- @return ListdefinedItem[]: keymap positions |ListdefinedItem|
 --- @return nil|string: error message
 function M.keymap(paths)
   return require("listdefined.command").collect(paths, "keymap")
@@ -12,7 +17,7 @@ end
 --- Returns autocmd positions defined in the files.
 --- This function targets |vim.api.nvim_create_autocmd()|.
 --- @param paths string[]: file paths
---- @return table: autocmd positions {path: string, start_row: number, text: string}[]
+--- @return ListdefinedItem[]: autocmd positions |ListdefinedItem|
 --- @return nil|string: error message
 function M.autocmd(paths)
   return require("listdefined.command").collect(paths, "autocmd")
@@ -21,7 +26,7 @@ end
 --- Returns autocmd group positions defined in the files.
 --- This function targets |vim.api.nvim_create_augroup()|.
 --- @param paths string[]: file paths
---- @return table: autocmd group positions {path: string, start_row: number, text: string}[]
+--- @return ListdefinedItem[]: autocmd group positions |ListdefinedItem|
 --- @return nil|string: error message
 function M.autocmd_group(paths)
   return require("listdefined.command").collect(paths, "autocmd_group")
@@ -30,7 +35,7 @@ end
 --- Returns highlight positions defined in the files.
 --- This function targets |vim.api.nvim_set_hl()|.
 --- @param paths string[]: file paths
---- @return table: highlight positions {path: string, start_row: number, text: string}[]
+--- @return ListdefinedItem[]: highlight positions |ListdefinedItem|
 --- @return nil|string: error message
 function M.highlight(paths)
   return require("listdefined.command").collect(paths, "highlight")
