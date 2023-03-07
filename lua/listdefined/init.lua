7667568005f2458f4013a842fb63ff56e319f7ca
notomo <notomo.motono@gmail.com>
2023-03-07 23:04:30 +0900

    Adjust @return doc

--- lua/listdefined/init.lua
+++ lua/listdefined/init.lua
@@ -8,8 +8,8 @@ local M = {}
 --- Returns keymap positions defined in the files.
 --- This function targets |vim.keymap.set()|.
 --- @param paths string[]: file paths
---- @return ListdefinedItem[]: keymap positions |ListdefinedItem|
---- @return nil|string: error message
+--- @return ListdefinedItem[] # keymap positions |ListdefinedItem|
+--- @return nil|string # error message
 function M.keymap(paths)
   return require("listdefined.command").collect(paths, "keymap")
 end
@@ -17,8 +17,8 @@ end
 --- Returns autocmd positions defined in the files.
 --- This function targets |vim.api.nvim_create_autocmd()|.
 --- @param paths string[]: file paths
---- @return ListdefinedItem[]: autocmd positions |ListdefinedItem|
---- @return nil|string: error message
+--- @return ListdefinedItem[] # autocmd positions |ListdefinedItem|
+--- @return nil|string # error message
 function M.autocmd(paths)
   return require("listdefined.command").collect(paths, "autocmd")
 end
@@ -26,8 +26,8 @@ end
 --- Returns autocmd group positions defined in the files.
 --- This function targets |vim.api.nvim_create_augroup()|.
 --- @param paths string[]: file paths
---- @return ListdefinedItem[]: autocmd group positions |ListdefinedItem|
---- @return nil|string: error message
+--- @return ListdefinedItem[] # autocmd group positions |ListdefinedItem|
+--- @return nil|string # error message
 function M.autocmd_group(paths)
   return require("listdefined.command").collect(paths, "autocmd_group")
 end
@@ -35,8 +35,8 @@ end
 --- Returns highlight positions defined in the files.
 --- This function targets |vim.api.nvim_set_hl()|.
 --- @param paths string[]: file paths
---- @return ListdefinedItem[]: highlight positions |ListdefinedItem|
---- @return nil|string: error message
+--- @return ListdefinedItem[] # highlight positions |ListdefinedItem|
+--- @return nil|string # error message
 function M.highlight(paths)
   return require("listdefined.command").collect(paths, "highlight")
 end
